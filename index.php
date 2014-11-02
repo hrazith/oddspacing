dfb5b8e49ec52402fcbca5c030679dc9ace44710
hrazith <hrazith@gmail.com>
2014-11-02 09:15:03 -0500

    Stratus content all in, issue #1

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html class="wf-freightdisplaypro-n7-active wf-freightdisplaypro-i3-active wf-freightdisplaypro-i7-active wf-freightdisplaypro-n3-active wf-active" xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Odd Spacing</title>
+
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link href="styles/main.css" rel="stylesheet" type="text/css">
+<link href="styles/reset.css" rel="stylesheet" type="text/css">
+<link href="styles/odd.css" rel="stylesheet" type="text/css">
+<link href="styles/fonts.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<script src="scripts/jquery-1.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/main.js" type="text/javascript" charset="utf-8"></script>
+<link href="styles/d.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Curtain -->
+
+	<div id="welcome">
+
+
+		<div class="intro">
+			<div class="brand">
+				<img src="images/oddman.svg" class="crimson" >
+			</div>
+			<h1><span class="gray">Odd Spacing</span> is the work of Designer,<br/> Creative Director <span class="crimson">Harun Razith</span>.</h1>
+			<p class="subtext">
+				Projects are broken into tenures, highlighting career episodes at various <br/>establishments where he has assumed UX and creative roles.
+			</p>
+			<div class="arrowDown"><span class="gray">SCROLL ↓ </span> </div>
+		</div>
+
+	</div>
+
+
+
+
+
+<!--  Nav -->
+
+	<?php include("_includes/nav.html");?>
+
+
+<!--  Mobile Nav  -->
+
+	<?php include("_includes/mobilenav.html");?>
+
+<!--  Menu -->
+
+	<?php include("_includes/menu.html");?>
+
+	
+
+<!--  Intro Area  -->
+
+
+	<div id="introContainerHome">
+
+		<div id="introCopy_container">
+			
+			<div id="introCopy">
+
+
+				<span class="crimson">UBS.</span>
+				
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+<!--  Project Area -->	
+
+
+	<div id="projectContainerHome" class="absolute">
+
+<!-- Ghost container -->
+
+	<div id="ghostContainer2">
+
+	</div>
+
+<!-- Des 1 -->
+
+		<div class="desContainer">
+
+			<h2> Build Portfolio </h2>
+			<ul class="credits">
+				<li class="role">Creative Direction & UX Architecture </li> 
+				<li class="research"> Michealnelo DiPlao & Micheal Grossman </li>
+			</ul>
+			
+
+			
+			<h3> Investment recommendations</h3>
+			<p>
+				Build Portfolio, Investment Recommendations is one of the first applications I designed at UBS. It is a tool financial analysts would use to efficiently create portfolio proposals and make detailed investment recommendations to their clients. 
+		  	</p>
+
+			<p>
+				The requriements dictated several complex interactions. We needed to design a simple and yet an effective way to create asset allocations, access firm's Intellectual Capital right from the tool (i.e. research lists), save specific securities and strategies as “favorites and include the ability to add assets held at UBS and outside.
+			</p>
+
+		</div>		
+
+<!-- Image 1 -->
+
+		<div class="projectImg_container">
+
+			<img src="images/bp1.png" class="projectImg">
+
+		</div>
+
+
+<!-- Des 2 -->
+
+		<div class="desContainer">
+
+			<p> Furhermore the tool had to have a feature to compare and contrast portfolio changes at the security and account level, visualize data in the form of charts and graphs.
+
+			</p>
+
+			
+		</div>		
+
+
+
+
+
+
+
+
+<!-- Image 2 -->
+
+		<div class="projectImg_container eop">
+
+			<img src="images/bp2.png" class="projectImg">
+
+		</div>
+
+
+<!-- Des 3 -->
+
+		<div class="desContainer">
+
+			<h2>BMSS</h2>
+			<ul class="credits">
+				<li class="role">Creative Direction & Interaction Design </li> 
+				<li class="research"> Gita Dubovis </li>
+			</ul>
+			<h3>Branch Manager Supervisory System</h3>
+
+			<p> BMSS is an tool that allows Control Officers to perform delegated supervision functions and approvals for the branch / complexes. They may also escalate supervisory issues to the Complex when necessary and follow-up on issues identified by complex supervisors. 
+
+			</p>
+
+		</div>		
+
+<!-- Image 3 -->
+
+		<div class="projectImg_container">
+
+			<img src="images/bmss1.png" class="projectImg">
+
+		</div>
+
+		<div class="desContainer">
+
+		
+			<p>
+				The system offers several ways to filter and sort through the lists. Users may then save these filters and use it at a later time..
+
+			</p>
+
+		</div>		
+
+<!-- Image 3 -->
+
+		<div class="projectImg_container">
+
+			<img src="images/bmss2.png" class="projectImg">
+
+		</div>
+
+<!-- Des 4 -->
+
+		<div class="desContainer">
+
+		
+			<p> Control officers can escalate trade violations by putting them in context whilst communications.
+
+			</p>
+
+		</div>
+
+		<div class="projectImg_container">
+
+			<img src="images/bmss3.png" class="projectImg">
+
+		</div>
+
+		<div class="desContainer eop">
+
+		
+			<p> Comments logged against accounts and trades.</p>
+
+		</div>
+
+		<div class="desContainer">
+
+			<h2> Financial Plan Billing</h2>
+			<ul class="credits">
+				<li class="role">Creative Direction & UX </li> 
+				<li class="research"> Gloria Petron  </li>
+			</ul>
+
+			<p>
+				A tool to manage billing for fee based Financial Plan engagements. 
+			</p>
+
+		</div>	
+		<div class="projectImg_container">
+
+			<img src="images/fpb1.png" class="projectImg">
+
+		</div>	
+		<div class="desContainer">
+
+		
+			<p> A view that shows accounts and its document, payment and engagement statuses.
+
+			</p>
+			
+		</div>
+		<div class="desContainer">
+			<h3> The look up tool</h3>
+			<p> Engagements can be searched by last name or tracking number. If an engagement is not found the system automatically initiates intake process and registers a new enagagement. </p>
+		</div>
+
+		<div class="projectImg_container">
+
+			<img src="images/fpb2.png" class="projectImg">
+
+		</div>
+		<div class="desContainer">
+			
+		
+			<p> The engagement then goes through the approval process where payments methods are setup and all the pertinent documents are collected, verified and previewed.
+
+			</p>
+
+		</div>
+		<div class="projectImg_container eop">
+
+			<img src="images/fpb3.png" class="projectImg">
+
+		</div>
+
+
+
+
+<!-- Bottom Navigation -->
+
+
+		<?php include("_includes/bottomnav.html");?>
+
+
+	
+
+<!-- FOOTER -->
+
+	<div id="footer">
+
+		<div id="footerCopy">
+
+			 © 2014   ODD SPACING_ 
+
+		</div>
+
+	</div>
+
+<!-- -->
+
+	</div>
+
+<!-- Project end -->
+
+<!-- Feature Image -->
+
+
+		<div id="introImg_container">
+
+			<div class="introImg">
+
+				<div class="pttrn">
+				</div>
+
+				<img src="images/ubs.jpg" class="introImg2">
+
+			</div>	
+
+		</div>
+
+
+</body></html>
---
+++ stratus.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html class="wf-freightdisplaypro-n7-active wf-freightdisplaypro-i3-active wf-freightdisplaypro-i7-active wf-freightdisplaypro-n3-active wf-active" xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Odd Spacing</title>
+
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link href="styles/main.css" rel="stylesheet" type="text/css">
+<link href="styles/reset.css" rel="stylesheet" type="text/css">
+<link href="styles/odd.css" rel="stylesheet" type="text/css">
+<link href="styles/fonts.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<script src="scripts/jquery-1.js" type="text/javascript" charset="utf-8"></script>
+<script src="scripts/main.js" type="text/javascript" charset="utf-8"></script>
+<link href="styles/d.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Curtain -->
+
+	
+
+
+
+<!--  Nav -->
+
+	<?php include("_includes/nav.html");?>
+
+<!--  Mobile Nav  -->
+
+	<?php include("_includes/mobilenav.html");?>
+
+<!--  Menu -->
+
+	<?php include("_includes/menu.html");?>	
+
+<!--  Intro Area  -->
+
+
+	<div id="introContainer">
+
+		<div id="introCopy_container">
+			
+			<div id="introCopy">
+
+
+				<span class="crimson">Stratus.</span>
+				
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+<!--  Project Area -->	
+
+
+	<div id="projectContainer" class="absolute">
+
+<!-- Ghost container -->
+
+	<div id="ghostContainer2">
+
+	</div>
+
+<!-- Des 1 -->
+
+		<div class="desContainer">
+
+			<h2> Avance </h2>
+			<ul class="credits">
+				<li class="role">Experience Architecture  & UI Engineering </li> 
+				<li class="research"> James Powers </li>
+			</ul>
+			
+
+			
+			<h3> Redesign of Software and Brand</h3>
+			<p>
+				Avance is a <a href="http://www.stratus.com/Products/Software/AvanceHA"> high avalibilty software </a> solution that combines two standard servers into one failsafe system. All data and applications are synchronized between the two servers continuously, in real time, ensuring critical applications are up and running.
+			</p>
+			
+
+			
+
+		</div>		
+
+<!-- Image 1 -->
+
+		<div class="projectImg_container">
+
+			<img src="images/stratus1.png" class="projectImg">
+
+		</div>
+
+
+<!-- Des 2 -->
+
+		<div class="desContainer">
+
+			<p>
+				My <b>heuristic evaluation</b> shed immediate light on several areas where the usability and UI experience could be improved: The software required a strong and a consistent brand, which would dictate a consistent visual style and information hierarchy across all states of UI interaction. Some areas of the UI failed to impart a clear sense of <b>workflow and navigational clarity</b>, and required redesign of its components. The product also required a sound <b>localization strategy</b> as many of its users were overseas. 
+				<img src="images/before.jpg">
+		  	</p>
+		  	
+		  	<p class="caption"> Avance before rebranding.</p>
+
+
+			
+		</div>		
+
+
+
+
+
+
+
+
+<!-- Image 2 -->
+
+		<div class="projectImg_container eop">
+
+			<img src="images/stratus2.png" class="projectImg">
+			<p class="caption"> Avance after rebranding.</p>
+
+
+		</div>
+
+		<div class="desContainer">
+			
+			<h3> Installation Experience </h3>
+			<p>
+				In its original form, the installation workflow failed to impart the desired user experience. There was no consistent navigation scheme applied across all the workflows, making it difficult for users to gauge which stage of the process they found themselves in at any given time. The UI provided no feedback for the occurrence of an important action. There were no clear visual or textual aids to guide the user in carrying out their workflows. 
+			</p>
+
+			<p>
+				 The solution is a workflow that is clear, simple, and consistent in navigation strategy.  
+			</p>
+			
+
+		</div>
+
+		<div class="projectImg_container">
+
+			<img src="images/stratus-install1.png" class="projectImg">
+			<img src="images/stratus-install2.png" class="projectImg">
+
+		</div>
+
+		<div class="desContainer">
+			
+			<h3> Snapshoting feature </h3>
+			<p>
+				This feature allows the 'shapshoting' or capturing a Virtual Machine at any given point in time. An interaction heavy feature that walks the user through all aspects of safely creating and exporting VMs and its volumes. 
+			</p>
+			
+
+		</div>
+		<div class="projectImg_container">
+
+			<img src="images/stratus-snapshot.png" class="projectImg">
+			
+
+		</div>
+
+		<div class="desContainer">
+			
+			<h3> VM Backup / Novice Mode </h3>
+			<p>
+				Archeticting the experience and the workflow of a VM backup for novice users. Dotted lines are process. Solid rectangles denote views in the UI. 
+			</p>
+			
+
+		</div>
+		<div class="projectImg_container eop">
+
+			<img src="images/backup-flow.svg" class="projectImg">
+			
+
+		</div>
+
+
+
+
+<!-- Des 3 -->
+
+		<div class="desContainer">
+
+			<h2>Multisite</h2>
+			<ul class="credits">
+				<li class="role">Creative Direction & Interaction Design </li> 
+				<li class="research"> James Powers </li>
+			</ul>
+			<h3>Remote access to your VMs</h3>
+
+			<p>  Multisite is an application that allows remote access to monitor and control the state of virtual machines, platforms and applications. 
+			</p>
+
+		</div>	
+		<div class="projectImg_container eop">
+
+			<img src="images/stratus-multisite1.png" class="projectImg">
+			<img src="images/stratus-multisite2.png" class="projectImg">
+
+		</div>	
+
+<!-- Image 3 -->
+
+
+
+
+
+
+
+<!-- Bottom Navigation -->
+
+<?php include("_includes/bottomnav.html");?>
+
+
+	
+
+<!-- FOOTER -->
+
+	<div id="footer">
+
+		<div id="footerCopy">
+
+			 © 2014   ODD SPACING_ 
+
+		</div>
+
+	</div>
+
+<!-- -->
+
+	</div>
+
+<!-- Project end -->
+
+<!-- Feature Image -->
+
+
+		<div id="introImg_container">
+
+			<div class="introImg">
+
+				<div class="pttrn">
+				</div>
+
+				<img src="images/stratus.jpg" class="introImg2">
+
+			</div>	
+
+		</div>
+
+
+</body></html>
